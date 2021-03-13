cc77c355905ed74e7d952adabd4ffa5812a683ea
JianWu <jian_dfh@hotmail.com>
2021-03-13 21:59:57 +0100

    update

--- utils.py
+++ utils.py
@@ -3,12 +3,12 @@ import pygame
 class playerMovement:
     def __init__(self):
         #Import Goku picture
-        self.gokuSurf = pygame.image.load('resources/imagens/player/goku/ss4/power.png')
+        self.gokuSurf = pygame.image.load('resources/imagens/player/goku/ss4/power.png').convert_alpha()
     
     def gokuRun(self, screen, x, y):
         #crop out picture from gokuSurf for goku run
-        self._gokuRunImg1 = self.gokuSurf.subsurface(6, 1, 34, 67).convert_alpha()
-        self._gokuRunImg2 = self.gokuSurf.subsurface(51, 1, 38, 67).convert_alpha()
+        self._gokuRunImg1 = self.gokuSurf.subsurface(6, 1, 34, 67)
+        self._gokuRunImg2 = self.gokuSurf.subsurface(51, 1, 38, 67)
         screen.blit(self._gokuRunImg1, (x,y))
         pygame.display.update()
         screen.blit(self._gokuRunImg2, (x,y))
