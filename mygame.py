5d1e58fcdf1de293c52cd5cf3e204ee7bed1c41f
unknown <wuoliver79@gmail.com>
2021-03-14 10:00:27 +0100

    bug fix on goku movement

--- mygame.py
+++ mygame.py
@@ -1,1 +1,26 @@
 import pygame
+import sys
+from utils import playerMovement
+import time
+
+pygame.init()
+
+
+screen = pygame.display.set_mode((1150, 1150))
+Background = pygame.image.load('resources/imagens/openning/background/goku-vs-vegeta-2.jpg')
+pm = playerMovement()
+screen.blit(Background, (0, 0))
+x = 0
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    x = x+1
+    screen.blit(Background, (0,0) )
+    pm.gokuRun(screen = screen, x = x, y= 100)
+    pygame.display.update()
+    time.sleep(0.1)
+
+
+
+ 
--- utils.py
+++ utils.py
@@ -3,15 +3,16 @@ import pygame
 class playerMovement:
     def __init__(self):
         #Import Goku picture
-        self.gokuSurf = pygame.image.load('resources/imagens/player/goku/ss4/power.png').convert_alpha()
+        self.gokuSurf = pygame.image.load('resources/imagens/player/goku/ss4/power.png').convert_alpha()     
+        self.gokuSurf = pygame.transform.scale2x(self.gokuSurf)
+        
     
     def gokuRun(self, screen, x, y):
+        factor = 2
         #crop out picture from gokuSurf for goku run
-        self._gokuRunImg1 = self.gokuSurf.subsurface(6, 1, 34, 67)
-        self._gokuRunImg2 = self.gokuSurf.subsurface(51, 1, 38, 67)
+        self._gokuRunImg1 = self.gokuSurf.subsurface(6*factor, 1*factor, 34*factor, 67*factor)
+        self._gokuRunImg2 = self.gokuSurf.subsurface(51*factor, 1*factor, 38*factor, 67*factor)
         screen.blit(self._gokuRunImg1, (x,y))
-        pygame.display.update()
         screen.blit(self._gokuRunImg2, (x,y))
-        pygame.display.update()
         
 
